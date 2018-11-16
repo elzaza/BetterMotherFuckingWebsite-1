f1cdef5be6110b8d3f7d14e93b4ae4c2b7fcb313
timothé <timothelecat94240@gmail.com>
2018-11-16 11:45:39 +0100

    ajout du fichier site.js

---
+++ site.js
@@ -0,0 +1,7 @@
+function fichierPopUp () {
+var confirme= confirm('allez je sais tu veux te vider dans des voisines chaudes de ta région,fais un effort! Clique sur <ok>') ;
+if (confirme) { window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ") ; }
+else { alert('ok ca va je comprends tu te degonfle') ; } }
+
+setTimeout(function() { fichierPopUp() } , 10000) ;
+
